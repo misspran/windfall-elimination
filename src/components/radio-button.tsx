098706b0d94c536503b97a693c2a3befc06baa4c
Vi Tran <vtran@cogolabs.com>
2019-09-17 20:03:22 -0400

    add radio button

---
+++ src/components/radio-button.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { colors } from "../constants";
+
+export const RadioButton = styled.input`
+  background-color: ${colors.white};
+  color: ${colors.black};
+  border: 2px solid ${colors.lightblue};
+  height: 25px;
+  width: 25px;
+  font-size: 50px;
+  border-radius: 25px;
+`;
